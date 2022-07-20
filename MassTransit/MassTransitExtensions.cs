9519fd3b72ffaa51048054ba02a1b52ee0bb1dba
Rahul Agarwal <talktorahulagarwal@gmail.com>
2022-07-20 20:03:14 +0530

    Add RabbitMQ Support

---
+++ MassTransit/MassTransitExtensions.cs
@@ -0,0 +1,29 @@
+﻿using MassTransit;
+using MassTransit.Definition;
+using Microsoft.Extensions.DependencyInjection;
+using Play.Catalog.Service.Settings;
+using Play.Common.Settings;
+using System.Reflection;
+
+namespace Play.Common.MassTransit
+{
+    public static class MassTransitExtensions
+    {
+        public static IServiceCollection AddMassTransitWithRabbitMQ(this IServiceCollection services, RabbitMQSettings rabbitMQSettings, ServiceSettings serviceSettings)
+        {
+            
+            services.AddMassTransit(ServiceProvider =>
+            {
+                ServiceProvider.AddConsumers(Assembly.GetEntryAssembly());
+                ServiceProvider.UsingRabbitMq((context, configurator) =>
+                {
+                    configurator.Host(rabbitMQSettings.Host);
+                    configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(serviceSettings.ServiceName, false));
+                });
+            });
+
+            services.AddMassTransitHostedService();
+            return services;
+        }
+    }
+}
